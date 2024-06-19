56303d76ade42c54e6b7eec478c120ba89e22d6f
CoenDV <c.oen@hotmail.nl>
2024-06-19 11:58:19 +0200

    init

---
+++ OudNieuw/src/main/java/com/CoenDV/OudNieuw/OudNieuwApplication.java
@@ -0,0 +1,13 @@
+package com.CoenDV.OudNieuw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OudNieuwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OudNieuwApplication.class, args);
+	}
+
+}
---
+++ OudNieuw/src/test/java/com/CoenDV/OudNieuw/OudNieuwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CoenDV.OudNieuw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OudNieuwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
